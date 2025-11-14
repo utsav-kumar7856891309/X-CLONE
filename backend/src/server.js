3aa54b59b291ad01fbb9b2941daed6b9cf52e8c1
utsav@kumar <15102004utsavkumar@gmail.com>
2025-11-14 21:39:51 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,14 +6,20 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+
 const app=express();
 app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
-
+app.use(arcjetMiddleware);
 app.use("/api/users", userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
   res.status(500).json({ error: err.message || "Internal server error" });
